ba1a8a23c89275e2528f56f24e47c55a92cdc657
Hannah Kiernan <2542983@dundee.ac.uk>
2023-11-18 13:37:36 +0000

    Fetch now returns a value

--- assembler/instructionCycle.cpp
+++ assembler/instructionCycle.cpp
@@ -4,16 +4,16 @@
 
 using namespace std;
 
-void fetch(int ci, ifstream store){
+string fetch(int ci, ifstream store){
     string instruction;
     for(int lineNumber = 0; getline(store,instruction) && lineNumber<= ci; lineNumber++){
         if(lineNumber == ci){
-            decode(instruction);
+            return instruction;
         }
     }
 }
 
-array<int,2> decode(string instruction){
+array<string,2> decode(string instruction){
     //First five characters of binary are line of store to be operated on, next 8 are ignored, next 3 are instruction, rest are ignored
     //000 - JMP S (Jump to instruction obtained from memory address S)
     //100 - JRP S (Jump to instruction at the program counter plus relative value from memory address S) 
@@ -22,6 +22,8 @@ array<int,2> decode(string instruction){
     //001 or 101 - SUB S (Subtract the number at memory address S from value in accumulator, store in accumulator)
     //011 - CMP (Skip if accumulator is negative)
     //111 - STP (Stop)
+
+    char* splitInstruct = instruction.data();
 }
 
 void execute(int accumulator, array<int,2> instruction){
