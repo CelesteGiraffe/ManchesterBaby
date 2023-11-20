72b8b6da089fcc126f93bbbfdeed921176b4f83f
Celeste Artley <90732656+CelesteGiraffe@users.noreply.github.com>
2023-11-20 22:49:35 +0000

    Return a Token by taking in a string

--- Assembler/lexer.cpp
+++ Assembler/lexer.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 
 enum class TokenType {
     Mnemonic, Operand, Label, Directive, Separator, Comment, EndOfLine
@@ -8,3 +9,7 @@ struct Token {
     TokenType type;
     std::string text;
 };
+
+std::vector<Token> lex(const std::string& input) {
+    std::vector<Token> tokens;
+}
