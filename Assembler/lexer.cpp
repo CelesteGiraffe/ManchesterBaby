a3a22e678e05049018009f4b271ffe5bb6e51d40
Celeste Artley <90732656+CelesteGiraffe@users.noreply.github.com>
2023-11-22 02:49:00 +0000

    updated lexer
    updated the lexer to check for if there is a label

--- Assembler/lexer.cpp
+++ Assembler/lexer.cpp
@@ -25,10 +25,26 @@ std::vector<Token> lex(const std::string &input)
     std::string tokenText;
     // Set's the default type to mnemonic
     TokenType currentType = TokenType::Mnemonic;
-    // Some lodgic to get tokens
+    bool isLabelStart = true;
+
     for (char ch : input)
     {
-        if (ch == ' ' || ch == '\t')
+        // Checks for if there is a Label which is preceded by the : only once.
+        if (isLabelStart && ch != ' ' && ch != '\t')
+        {
+            if (ch == ':')
+            {
+                if (!tokenText.empty())
+                {
+                    tokens.push_back({TokenType::Label, tokenText});
+                    tokenText.clear();
+                }
+                isLabelStart = false;
+                continue;
+            }
+        }
+        // Checks for a tab or space to set the operands
+        else if (ch == ' ' || ch == '\t')
         {
             if (!tokenText.empty())
             {
@@ -37,6 +53,7 @@ std::vector<Token> lex(const std::string &input)
             }
             currentType = TokenType::Operand;
         }
+        // Ignores the characters after the ; to denote comments.
         else if (ch == ';')
         {
             if (!tokenText.empty())
@@ -46,8 +63,10 @@ std::vector<Token> lex(const std::string &input)
             }
             currentType = TokenType::Comment;
         }
+        // Set's the EndOfLine token type
         else if (ch == '\n')
         {
+            isLabelStart = true;
             if (!tokenText.empty())
             {
                 tokens.push_back({currentType, tokenText});
@@ -56,6 +75,7 @@ std::vector<Token> lex(const std::string &input)
             tokens.push_back({TokenType::EndOfLine, "\n"});
             currentType = TokenType::Mnemonic;
         }
+        // Adds the token text after each type to determine what to do.
         else
         {
             tokenText += ch;
