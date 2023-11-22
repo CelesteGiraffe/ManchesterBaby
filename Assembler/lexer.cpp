c5c84bf55fad3a39f5895170e41adc19910024b3
Celeste Artley <90732656+CelesteGiraffe@users.noreply.github.com>
2023-11-22 03:14:39 +0000

    Refactored code

--- Assembler/lexer.cpp
+++ Assembler/lexer.cpp
@@ -21,6 +21,8 @@ struct Token
 // pass by refrence with &
 std::vector<Token> lex(const std::string &input)
 {
+    // Example Command: START:    LDN NUM01   ; Copy variable to accumulator (negated)
+
     std::vector<Token> tokens;
     std::string tokenText;
     // Set's the default type to mnemonic
@@ -29,53 +31,14 @@ std::vector<Token> lex(const std::string &input)
 
     for (char ch : input)
     {
-        // Checks for if there is a Label which is preceded by the : only once.
-        if (isLabelStart && ch != ' ' && ch != '\t')
+        if (handleLabel(ch, tokenText, tokens, isLabelStart) ||
+            handleSeparator(ch, tokenText, tokens, currentType) ||
+            handleSpaceOrTab(ch, tokenText, tokens, currentType) ||
+            handleComment(ch, tokenText, tokens, currentType) ||
+            handleNewLine(ch, tokenText, tokens, currentType, isLabelStart))
         {
-            if (ch == ':')
-            {
-                if (!tokenText.empty())
-                {
-                    tokens.push_back({TokenType::Label, tokenText});
-                    tokenText.clear();
-                }
-                isLabelStart = false;
-                continue;
-            }
+            continue; // Skip the rest of the loop if any condition was handled
         }
-        // Checks for a tab or space to set the operands
-        else if (ch == ' ' || ch == '\t')
-        {
-            if (!tokenText.empty())
-            {
-                tokens.push_back({currentType, tokenText});
-                tokenText.clear();
-            }
-            currentType = TokenType::Operand;
-        }
-        // Ignores the characters after the ; to denote comments.
-        else if (ch == ';')
-        {
-            if (!tokenText.empty())
-            {
-                tokens.push_back({currentType, tokenText});
-                tokenText.clear();
-            }
-            currentType = TokenType::Comment;
-        }
-        // Set's the EndOfLine token type
-        else if (ch == '\n')
-        {
-            isLabelStart = true;
-            if (!tokenText.empty())
-            {
-                tokens.push_back({currentType, tokenText});
-                tokenText.clear();
-            }
-            tokens.push_back({TokenType::EndOfLine, "\n"});
-            currentType = TokenType::Mnemonic;
-        }
-        // Adds the token text after each type to determine what to do.
         else
         {
             tokenText += ch;
@@ -87,4 +50,85 @@ std::vector<Token> lex(const std::string &input)
         tokens.push_back({currentType, tokenText});
     }
     return tokens;
+}
+
+bool handleLabel(char ch, std::string &tokenText, std::vector<Token> &tokens, bool &isLabelStart)
+{
+    if (isLabelStart && ch != ' ' && ch != '\t')
+    {
+        if (ch == ':')
+        {
+            if (!tokenText.empty())
+            {
+                tokens.push_back({TokenType::Label, tokenText});
+                tokenText.clear();
+            }
+            isLabelStart = false;
+            return true; // Label was handled
+        }
+    }
+    return false; // No label to handle
+}
+
+bool handleSeparator(char ch, std::string &tokenText, std::vector<Token> &tokens, TokenType &currentType)
+{
+    if (ch == ',')
+    {
+        if (!tokenText.empty())
+        {
+            tokens.push_back({currentType, tokenText});
+            tokenText.clear();
+        }
+        tokens.push_back({TokenType::Separator, ","});
+        currentType = TokenType::Operand;
+        return true;
+    }
+    return false;
+}
+
+bool handleSpaceOrTab(char ch, std::string &tokenText, std::vector<Token> &tokens, TokenType &currentType)
+{
+    if (ch == ' ' || ch == '\t')
+    {
+        if (!tokenText.empty())
+        {
+            tokens.push_back({currentType, tokenText});
+            tokenText.clear();
+        }
+        currentType = TokenType::Operand;
+        return true;
+    }
+    return false;
+}
+
+bool handleComment(char ch, std::string &tokenText, std::vector<Token> &tokens, TokenType &currentType)
+{
+    if (ch == ';')
+    {
+        if (!tokenText.empty())
+        {
+            tokens.push_back({currentType, tokenText});
+            tokenText.clear();
+        }
+        currentType = TokenType::Comment;
+        return true;
+    }
+    return false;
+}
+
+bool handleNewLine(char ch, std::string &tokenText, std::vector<Token> &tokens, TokenType &currentType, bool &isLabelStart)
+{
+    if (ch == '\n')
+    {
+        isLabelStart = true;
+        if (!tokenText.empty())
+        {
+            tokens.push_back({currentType, tokenText});
+            tokenText.clear();
+        }
+        tokens.push_back({TokenType::EndOfLine, "\n"});
+        currentType = TokenType::Mnemonic;
+        return true;
+    }
+    return false;
 }
