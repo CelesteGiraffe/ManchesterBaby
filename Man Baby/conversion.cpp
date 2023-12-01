7025c6a47d7f852e0dd3d7ba755a4a4ce40d42fb
Hannah Kiernan <2542983@dundee.ac.uk>
2023-12-01 03:36:14 +0000

    Function for getting inverse of binary number

--- Man Baby/conversion.cpp
+++ Man Baby/conversion.cpp
@@ -3,52 +3,90 @@
 
 using namespace std;
 
-string binToDec(string bin){
+string binToDec(string bin)
+{
     int sum;
     int binaryMult = 1;
     int hold;
+    int sign = 1;
 
+    if (bin[0] == 1)
+    {
+        sign = -1;
+        bin = getNegative(bin);
+    }
+    for (int i = bin.length() - 1; i > 0; i--)
+    {
+        hold = (int)bin[i];
+        hold *= binaryMult;
+        sum += hold;
+        binaryMult *= 2;
+    }
 
-        for(int i = bin.length() - 1; i > 0; i--){
-            hold = (int)bin[i];
-            hold *= binaryMult;
-            sum += hold;
-            binaryMult *= 2;
-        }
-        return to_string(sum);
+    sum *= sign;
+    return to_string(sum);
 }
 
-string binToMne(string bin){
+string binToMne(string bin)
+{
 
     int binInt = stoi(bin);
 
-    switch(binInt){
-        case 0:
-            return "JMP";
+    switch (binInt)
+    {
+    case 0:
+        return "JMP";
 
-        case 100:
-            return "JRP";
-        
-        case 10:
-            return "LDN";
-        
-        case 110:
+    case 100:
+        return "JRP";
 
-            return "STO";
-        
-        case 1:
-        case 101:
+    case 10:
+        return "LDN";
 
-            return "SUB";
+    case 110:
 
-        case 11:
+        return "STO";
 
-            return "CMP";
+    case 1:
+    case 101:
 
-        case 111:
+        return "SUB";
 
-            return "STP";
-        
+    case 11:
+
+        return "CMP";
+
+    case 111:
+
+        return "STP";
     }
-    
+}
+
+string getNegative(string twosComp)
+{
+
+    string hold;
+
+    for (int i = 0; i < twosComp.length() - 1; i++)
+    {
+        if (twosComp[i] == '0')
+        {
+            hold.append("1");
+        }
+        else
+        {
+            hold.append("0");
+        }
+    }
+
+    if (twosComp[twosComp.length()] == '0')
+    {
+        hold.append("0");
+    }
+    else
+    {
+        hold.append("1");
+    }
+
+    return hold;
 }
--- Man Baby/conversion.hpp
+++ Man Baby/conversion.hpp
@@ -3,3 +3,4 @@ using namespace std;
 
 string binToDec(string bin);
 string binToMne(string bin);
+string getNegative(string twosComp);
